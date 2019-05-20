0c0af5cdcde7bf2d93912f8d76c8d5718eff91bb
Ricardo Oncoy <ricardox_2@hotmail.com>
2019-05-20 15:20:06 -0500

    Actualizado al final de la clase

--- funciones.php
+++ funciones.php
@@ -1,18 +1,36 @@
 <?php 
 	function nombreCompleto($nom, $pat, $mat){
-		return trim($nom)." ".trim($pat)." ".trim($mat);
+		$apenom = trim($nom)." ".trim($pat)." ".trim($mat);
+		return strtoupper($apenom);
+	}
+
+
+
+	function generaCorreo($nom, $pat){
+		$email="";
+		$email=substr(trim($nom), 0,1).substr(eliminaEspacios($pat), 0)."@iegb.edu.pe";
+		return strtolower($email);
 	}
 
 	function generaCodigo($nom, $pat, $mat){
 		$codigo="";
-		$codigo=substr(trim($nom),0,1).substr($pat, 0,1).substr($mat, 0,1)."-001";
+		$codigo=substr(trim($nom),0,1).substr(trim($pat), 0,1).substr(trim($mat),0,1)."-001";
 		return $codigo;
 	}
+	function eliminaEspacios($str){
+		$str=trim($str); 
+		$cad="";
+		for ($i=1; $i < strlen($str) ; $i++) { 
+			$cad=substr($i,$i+1);
 
-	function generaCorreo($nom, $pat){
-		$email="";
-		$email=substr($nom, 0,1).substr($pat, 0)."@iegb.edu.pe";
-		return $email;
+			if($cad==' '){
+				$str=substr($str,0,$i).substr($str, $i+2, strlen($str));
+			}
+		}
+		return $str;
 	}
+
+
+
 
  ?>
